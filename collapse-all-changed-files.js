1109cbda455d39e0f20842a1059ddb35950a97f4
ThomasLesinski <tlesinski@web.de>
2021-02-27 19:09:48 +0100

    Create collapse-all-changed-files.js

---
+++ collapse-all-changed-files.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         GH Shopware - Collapse all changed files
+// @namespace    https://www.ottscho-it-service.de/
+// @version      1
+// @description  Add btn to collapse all changed files in pullrequest "changed files" tab
+// @author       ThomasLesinski
+// @include      https://github.com/*/pull/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+    const addCollapseAllBtnInterval = setInterval(() => {
+        const ourBtn = document.querySelector('.btn-collapse-all-changed-files');
+
+        if (!ourBtn) {
+            const anchor = document.querySelector('.diffbar-item.details-reset.details-overlay.position-relative.text-center.hide-sm.hide-md');
+
+            if (anchor) {
+                const collapseAllBtn = document.createElement('div');
+
+                collapseAllBtn.classList.add('btn', 'ml-5', 'btn-collapse-all-changed-files');
+                collapseAllBtn.innerHTML = 'Collapse all';
+
+                collapseAllBtn.addEventListener('click', () => {
+                    const collapseArrowBtns = document.querySelectorAll('.js-file.open .file-info button[aria-expanded="true"]');
+
+                    collapseArrowBtns.forEach((collapseArrowBtn) => {
+                        collapseArrowBtn.click();
+                    });
+                });
+
+                anchor.parentElement.appendChild(collapseAllBtn);
+            }
+        }
+    }, 250);
+})();
